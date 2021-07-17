b973c7917e672a2156bb513fa576cfcef5000ec1
itachi176 <lhoang17062000@gmail.com>
2021-07-17 22:47:10 +0700

    fix url

--- qr.py
+++ qr.py
@@ -2,8 +2,8 @@ import pyqrcode
 import sys
 import png
 from pyqrcode import QRCode
-url = pyqrcode.create('http://uca.edu')
+url = pyqrcode.create('https://linktr.ee/minhhoang176')
 # url.svg(sys.stdout, scale=1)
 # url.svg('uca.svg', scale=4)
 # number = pyqrcode.create(123456789012345)
-url.png('big-number.png', scale = 6)
+url.png('profile', scale = 6)
